dc93ba4a8b0106356ebca1b05a44bf42108da036
malexj <alexmaximovj@gmail.com>
2016-04-30 18:19:25 +0300

    Initial commit

---
+++ src/main/java/com/epam/init/Main.java
@@ -0,0 +1,25 @@
+package com.epam.init;
+
+import com.epam.entity.Cake;
+import com.epam.entity.Candy;
+import com.epam.entity.box.Box;
+import com.epam.service.PrintService;
+
+public class Main {
+
+    private static PrintService ser = new PrintService();
+
+    public static void main(String[] args) {
+        Box box = new Box();
+        box.addSweet(new Cake("cake111", 2.1, 5));
+        box.addSweet(new Cake("cake222", 2.6, 7));
+        box.addSweet(new Candy("can111", "type1", "black", 3.6, 8));
+        box.addSweet(new Candy("can222", "type2", "red", 8, 5));
+        box.addSweet(new Candy("can333", "type3", "blue", 45, 1));
+
+        ser.print("Sweets", box.getAllSweets());
+        ser.print("total weigh", box.getWeigh());
+        ser.print("count type sweets", box.countTypeSweets());
+        ser.print("all count sweets", box.getCountAllSweets());
+    }
+}
